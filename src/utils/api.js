9557bb8801e30feac164f91bbda9e47f005257fb
Abul kaish <webdeveloper.sisgain@gmail.com>
2023-08-26 14:22:05 +0530

    youtube-clone

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            process.env.REACT_APP_YOUTUBE_API_KEY || "5b1715bd54msha7f5f1d84706138p17a44fjsnef0cedfe4ddd",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
